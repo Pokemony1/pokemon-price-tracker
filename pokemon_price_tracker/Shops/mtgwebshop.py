172363beb7ce6e00526af9cb2a885d49abc1867c
Pokemony1 <djurkj@hotmail.com>
2026-02-18 18:32:45 +0100

    Centralize search queries and add SV 151 + Mega Evolution

--- pokemon_price_tracker/Shops/mtgwebshop.py
+++ pokemon_price_tracker/Shops/mtgwebshop.py
@@ -1,9 +1,8 @@
 from pokemon_price_tracker.shopify_scraper import scan_shopify_store_json
+from pokemon_price_tracker.queries import QUERIES
 
 SHOP_NAME = "mtgwebshop"
 DOMAIN = "mtgwebshop.dk"
-QUERIES = ["crown zenith", "prismatic evolution"]
-
 
 def get_products():
     return scan_shopify_store_json(DOMAIN, QUERIES)
---
+++ pokemon_price_tracker/queries.py
@@ -0,0 +1,9 @@
+QUERIES = [
+    "crown zenith",
+    "prismatic evolution",
+    "scarlet & violet 151",
+    "sv 151",
+    "pokemon 151",
+    "mega evolution",
+    "mega evolutions",
+]
